11b692c10b4feff040ec64739a7eab53cbed2734
Hryhoriy <h.kysil@grt-team.com>
2024-02-25 18:28:18 +0200

    added scripts for parcing page.

---
+++ script/scraper.rb
@@ -0,0 +1,44 @@
+require "httparty" 
+require "nokogiri"
+
+# defining a data structure to store the scraped data 
+Retailer = Struct.new(:url, :name,)
+
+
+response = HTTParty.get("https://en.wikipedia.org/wiki/Category:Online_retailers_of_the_United_States")
+
+#puts response
+
+# parsing the HTML document returned by the server 
+document = Nokogiri::HTML(response.body)
+
+# selecting all HTML product elements 
+pages = document.css("#mw-pages")
+
+items = pages.css("li")
+
+itemList = []
+
+items.each do |item| 
+	# extracting the data of interest 
+	# from the current product HTML element 
+	url = item.css("a").first.attribute("href").value 
+	name = item.css("a").text
+ 
+	# storing the scraped data in a PokemonProduct object 
+	retailer = Retailer.new(url, name) 
+ 
+	# adding the PokemonProduct to the list of scraped objects 
+    itemList.push(retailer) 
+end
+
+csv_headers = ["url", "name"] 
+CSV.open("output.csv", "wb", write_headers: true, headers: csv_headers) do |csv| 
+	# adding each pokemon_product as a new row 
+	# to the output CSV file 
+	itemList.each do |item| 
+		csv << item 
+	end 
+end
+
+puts itemList
